e2b30b67c8556694a49adb62d34f0246f2b65fea
unknown <gusta.antonius@gmail.com>
2025-11-10 21:07:17 -0300

    Microkernel.

---
+++ microkernel-kernel/src/main/java/br/univille/microkernel_kernel/controller/HomeController.java
@@ -0,0 +1,30 @@
+package br.univille.microkernel_kernel.controller;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.univille.microkernel_interface.service.DefaultService;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @Autowired
+    private Map<String, DefaultService> services;
+
+    @GetMapping
+    public ResponseEntity<String> index() {
+        String serviceList = Arrays.toString(services.keySet().toArray());
+
+        for (var umService : services.values()) {
+            umService.doWork(null);
+        }
+
+        return ResponseEntity.ok().build();
+    }
+}
